39e1742180ef98ed4f8107a03f718cb8081b74cd
Saul-Ortega <saulalbertoortegarodriguez@gmail.com>
2025-10-13 10:21:20 +0100

    Modify code to render App component

--- miapp/src/main.jsx
+++ miapp/src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import AppGrid from './AppGrid.jsx'
+//import AppGrid from './AppGrid.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/*<App />*/}
-    <AppGrid />
+    <App />
+    {/*<AppGrid />*/}
   </StrictMode>,
 )
